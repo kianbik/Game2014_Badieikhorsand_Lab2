0835310ab21e64ac0a561e4f3dccf983fac91503
Kian <kianbadiei@gmail.com>
2021-10-01 21:10:56 -0400

    Wrong template added. Fixed the issue

--- Assets/[Scripts]/StartButtonBehaviour.cs
+++ Assets/[Scripts]/StartButtonBehaviour.cs
@@ -1,12 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class StartButtonBehaviour : MonoBehaviour
 {
     public void OnStartButtonPressed()
     {
-        SceneManager.LoadScene("Main");
+        Debug.Log("Start Button Pressed!");
     }
 }
